bcdd9e44b7fcdca5f6b33bf5f2feb6ae659ba441
Джалоладдин <joniatabekovich07@gmail.com>
2026-05-23 19:54:35 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
 #include "bst.h"
 
+void makeTree(BST<std::string>& tree, const char* filename);
+
 int main() {
+    BST<std::string> tree;
+
+    makeTree(tree, "warpeace.txt");
+
+    std::cout << "Depth: " << tree.depth() << std::endl;
+
     return 0;
 }
