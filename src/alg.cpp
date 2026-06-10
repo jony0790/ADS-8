34ebde41f1ece71ceaa2291ce2aa6eb517c522d8
Джалоладдин <joniatabekovich07@gmail.com>
2026-06-10 05:29:39 +0300

    Джумабаев Джалоладдин 25-ИСТ-1-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,34 @@
-// Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+// Copyright 2026
+
+#include <cctype>
+#include <fstream>
+#include <string>
+
+#include "../include/bst.h"
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+
+    if (!file)
+        return;
+
+    std::string word;
+    char ch;
+
+    while (file.get(ch)) {
+        if (std::isalpha(static_cast<unsigned char>(ch))) {
+            word += static_cast<char>(
+                std::tolower(static_cast<unsigned char>(ch)));
+        } else {
+            if (!word.empty()) {
+                tree.insert(word);
+                word.clear();
+            }
+        }
+    }
+
+    if (!word.empty())
+        tree.insert(word);
+
+    file.close();
 }
